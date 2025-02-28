a3e3c423028aa053c923f74e4d1c5ad59841842b
Hakjun Kim <125016134+junkmm@users.noreply.github.com>
2025-02-28 15:48:22 +0900

    Update hello.go

--- Week1/hello.go
+++ Week1/hello.go
@@ -1,1 +1,16 @@
-1
+package main
+
+import (
+        "fmt"
+        "log"
+        "net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello World!!!!!!23434234")
+}
+
+func main() {
+        http.HandleFunc("/", index)
+        log.Fatal(http.ListenAndServe(":80", nil))
+}
